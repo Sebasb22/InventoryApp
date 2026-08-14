b51156cd7a88a90cecf318c8dfda7a0e7b6fa0d6
Sebasb22 <sebastian.bolanos_c@uao.edu.co>
2026-08-14 18:43:36 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"Inventory Management Application Version: {version}");
